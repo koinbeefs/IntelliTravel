1bc73e69ff02033705f28854ecc317e5f01e7643
koinbeefs <137254116+koinbeefs@users.noreply.github.com>
2026-01-13 15:59:14 +0800

    Add unix_socket configuration to database.php

--- config/database.php
+++ config/database.php
@@ -53,6 +53,7 @@ return [
             'password' => env('DB_PASSWORD', ''),
             'charset' => env('DB_CHARSET', 'utf8mb4'),
             'collation' => env('DB_COLLATION', 'utf8mb4_unicode_ci'),
+            'unix_socket' => env('DB_SOCKET'),
             'prefix' => '',
             'prefix_indexes' => true,
             'strict' => true,
